93f91ac5ca98aee4b811cde1abcd7b1ecf906496
FoxHound112263 <camayorquina@unal.edu.co>
2019-05-28 16:02:47 -0500

    Iteration for only one variable

--- vector-algorithm.R
+++ vector-algorithm.R
@@ -15,4 +15,33 @@ separate[[1]][[1]]
 
 mapply(function(n,v) which(separate[[n]]==v) , d1$name,d1$val)
 
-pivote[4:nrow(pivote), separate[[1]] ]
+example <-  pivote[4:nrow(pivote), separate[[1]] ]
+split(example, seq_along(1:10))
+
+library(stringi)
+pattern <- unlist(stringi::stri_extract_all_regex(vector$questions[1],"[0-9]+") )
+split(example,  rep(length(unlist(uniques)),13)  )
+unique(pattern)
+
+uniques <-  t(as.matrix(table(pattern)))
+
+
+container <-  sapply(unique(pattern),
+              function(x) example[startsWith(names(example) , x )],
+              simplify = FALSE)
+
+container <- lapply(container, function(x) as.data.frame(sapply(x, as.numeric))) 
+
+
+result <- rowSums(container[[1]])
+
+result <- NULL
+for (dataframe in 1:length(container)) {
+  result[[dataframe]] = ( rowSums(container[[dataframe]]) / ncol(container[[dataframe]]) )
+  
+}
+
+
+
+
+uniques <- uniques[1:length(separate)]
