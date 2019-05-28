15aebc249e1cbc39b71e2c8853dd12b061df7a12
FoxHound112263 <camayorquina@unal.edu.co>
2019-05-28 17:02:28 -0500

    Attempting 'for': special case detected

--- vector-algorithm.R
+++ vector-algorithm.R
@@ -1,4 +1,5 @@
 require(readxl)
+library(stringi)
 # Work directory
 directory <- 'C:/Users/ScmayorquinS/OneDrive - Departamento Nacional de Planeacion/DIDE/2019/Data Science Projects/Innovation-Index-Algorithms/'
 
@@ -9,39 +10,71 @@ colnames(vector) <- c('variable','questions')
 # Pivote table with desired columns
 pivote <- readxl::read_excel(paste0(directory, 'Pivote.xlsx'))
 
+#----------------------------------------------------------------------------------
 # First try
-separate <- as.list(strsplit(vector$questions, ";")) 
-separate[[1]][[1]]
+#----------------------------------------------------------------------------------
 
-mapply(function(n,v) which(separate[[n]]==v) , d1$name,d1$val)
+# List of all the variables
+separate <- as.list(strsplit(vector$questions, ";")) 
+
 
 example <-  pivote[4:nrow(pivote), separate[[1]] ]
-split(example, seq_along(1:10))
 
-library(stringi)
+# numeric pattern to separate questions of same variable
 pattern <- unlist(stringi::stri_extract_all_regex(vector$questions[1],"[0-9]+") )
-split(example,  rep(length(unlist(uniques)),13)  )
-unique(pattern)
 
-uniques <-  t(as.matrix(table(pattern)))
-
-
+# store dataframes of questions in a list
 container <-  sapply(unique(pattern),
               function(x) example[startsWith(names(example) , x )],
               simplify = FALSE)
 
+# convert character dataframes into numeric
 container <- lapply(container, function(x) as.data.frame(sapply(x, as.numeric))) 
 
-
-result <- rowSums(container[[1]])
-
+# store means in list
 result <- NULL
 for (dataframe in 1:length(container)) {
+  # sum of values of column per question / total of questions in this variable
   result[[dataframe]] = ( rowSums(container[[dataframe]]) / ncol(container[[dataframe]]) )
+}
+
+# final mean
+final <- Reduce("+", result) / length(result)
+
+#----------------------------------------------------------------------------------
+# For attempt
+#----------------------------------------------------------------------------------
+
+# List of all the variables
+separate <- as.list(strsplit(vector$questions, ";")) 
+
+for (question in 1:length(separate)) {
+  example <-  pivote[4:nrow(pivote), separate[[question]] ]
+  
+  # numeric pattern to separate questions of same variable
+  pattern <- unlist(stringi::stri_extract_all_regex(vector$questions[question],"[0-9]+") )
   
 }
 
+pattern <- unlist(stringi::stri_extract_all_regex(vector$questions[1],"[0-9]+") )
 
 
 
-uniques <- uniques[1:length(separate)]
+# store dataframes of questions in a list
+container <-  sapply(unique(pattern),
+                     function(x) example[startsWith(names(example) , x )],
+                     simplify = FALSE)
+
+# convert character dataframes into numeric
+container <- lapply(container, function(x) as.data.frame(sapply(x, as.numeric))) 
+
+# store means in list
+result <- NULL
+for (dataframe in 1:length(container)) {
+  # sum of values of column per question / total of questions in this variable
+  result[[dataframe]] = ( rowSums(container[[dataframe]]) / ncol(container[[dataframe]]) )
+}
+
+# final mean
+final <- Reduce("+", result) / length(result)
+
