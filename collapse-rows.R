0c889024c004d4553e32c621a2669a7b38fd53f7
FoxHound112263 <camayorquina@unal.edu.co>
2019-05-30 12:03:17 -0500

    task finished, but waiting for special cases

--- collapse-rows.R
+++ collapse-rows.R
@@ -4,13 +4,12 @@ directory <-  'C:/Users/ScmayorquinS/OneDrive - Departamento Nacional de Planeac
 
 data <- readxl::read_excel(paste0(directory, 'data/EDI_Entidad_2018.xlsx')  )
 
+data2 <-  aggregate(data[, 3:ncol(data)], list(data$CODENT), mean)
 
-library(data.table)
-data2 <-  aggregate(data[, 2:ncol(data)], list(data$CODENT), mean)
-
-uniques <-  unique(c(data$CODENT,data$CUIN))
+#uniques <-  unique(c(data$CODENT,data$CUIN))
 
 data3 <- data2[,2:ncol(data2)]
+
 # Custom formula
 
 rule <-  function(x) {
@@ -18,7 +17,7 @@ rule <-  function(x) {
   return(n)
 }
 
-data3[] <- lapply(data3, rule)
+data3[] <- lapply(data3[2:ncol(data3)], rule)
 
-data4 <-  add_column(data3, data, .before = 1)
-
+data_final <-  add_column(data3, data2$Group.1, .before = 1)
+colnames(data_final)[which(names(data_final) == "data2$Group.1")] <- "CODENT"
