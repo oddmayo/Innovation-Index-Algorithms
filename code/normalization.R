8db2e6d02dcc0928051a98a1e2c6e635f5f0a861
FoxHound112263 <camayorquina@unal.edu.co>
2019-11-12 09:24:31 -0500

    Correcting via normalization

--- code/normalization.R
+++ code/normalization.R
@@ -3,22 +3,32 @@
 EDI <- openxlsx::read.xlsx("C:/Users/LcmayorquinL/OneDrive - Departamento Nacional de Planeacion/DIDE/2019/Data Science Projects/Innovation-Index-Algorithms/output/EDI_promedios.xlsx")
 
 
-for (column in ncol(EDI)) {
-  if (mean(EDI$column)) {
-    
-  }
-}
-
 columns <- colnames(EDI)
 columns <- columns[3:length(columns)]
 
-for (column in columns) {
-  temp_mean <-  mean(EDI[,column])
-  if (with(temp_mean, temp_mean >= 1 & temp_mean <2) ) {
-    temp_mean
-    }
+range_1 <- function(x){
+  n = 2-x
+  return(n)
 }
 
- 
+range_2 <-  function(x) {
+  n = ( (x-1)/4 )
+  return(n)
+}
 
-with(temp_mean, temp_mean >= 1 & temp_mean <2)
+empty_df = EDI[,FALSE]
+  
+  for (column in columns) {
+    temp_mean <-  mean(EDI[,column])
+    if (temp_mean >= 1 & temp_mean < 2) {
+      test <- lapply(EDI[,column], range_1)
+      empty_df[,column] <- unlist(test)
+      
+    } else if (temp_mean >= 2 & temp_mean <= 5) {
+        test <- lapply(EDI[,column], range_2)
+        empty_df[,column] <- unlist(test)
+      }
+  }
+
+  
+
