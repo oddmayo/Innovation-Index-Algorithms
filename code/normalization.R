1a1cb48a4994faab98e9b4914df87be4d47ed463
FoxHound112263 <camayorquina@unal.edu.co>
2019-11-12 09:42:16 -0500

    Output Excel file of normalized EDI means

--- code/normalization.R
+++ code/normalization.R
@@ -1,21 +1,28 @@
 # Normalize EDI means
 
+# Working directory
+directory <- 'C:/Users/LcmayorquinL/OneDrive - Departamento Nacional de Planeacion/DIDE/2019/Data Science Projects/Innovation-Index-Algorithms/'
+
+# Load EDI means
 EDI <- openxlsx::read.xlsx("C:/Users/LcmayorquinL/OneDrive - Departamento Nacional de Planeacion/DIDE/2019/Data Science Projects/Innovation-Index-Algorithms/output/EDI_promedios.xlsx")
 
-
+# columns to iterate through loop
 columns <- colnames(EDI)
 columns <- columns[3:length(columns)]
 
+# Case #1: if values are between 1 and 2 - [1,2)
 range_1 <- function(x){
   n = 2-x
   return(n)
 }
 
+# Case #2: if values are between 2 and 5 - [2,5]
 range_2 <-  function(x) {
   n = ( (x-1)/4 )
   return(n)
 }
 
+# Empty dataframe
 empty_df = EDI[,FALSE]
   
   for (column in columns) {
@@ -30,5 +37,13 @@ empty_df = EDI[,FALSE]
       }
   }
 
-  
+library(tibble)
+empty_df <- add_column(empty_df, EDI$CODENT, .before = 1)
 
+names(empty_df)[names(empty_df) == "EDI$CODENT"] <- "CODENT"
+
+EDI_means <- empty_df
+
+library(openxlsx)
+# Final EDI means
+openxlsx::write.xlsx(x = EDI_means, file = paste0(directory, "output/EDI_promedios_normalizada.xlsx"))
