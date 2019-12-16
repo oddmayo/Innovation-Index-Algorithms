42a4cc7d832b21108967ca9b54c7981ba508eff3
FoxHound112263 <camayorquina@unal.edu.co>
2019-12-16 17:01:30 -0500

    Special cases included, but fixing index

--- code/index_v1.R
+++ code/index_v1.R
@@ -18,7 +18,7 @@ global <- global[-c(1:3),]
 global[] <- lapply(global, function(x) as.numeric(as.character(x)))
 
 # Data with nested groups, which will be averaged
-EDI_dir <- paste0(directory, '/data/ICIP_V22.xlsx')
+EDI_dir <- paste0(directory, '/data/ICIP_V25.xlsx')
 
 # Entities names
 names <- read.xlsx(paste0(directory, '/output/pivote_nuevo.xlsx'))
@@ -29,7 +29,7 @@ colnames(names) <- c('código','entidad')
 # First dimension #
 #-----------------#
 
-EDI_1 <- read.xlsx(xlsxFile = EDI_dir, sheet = 2,fillMergedCells = T)
+EDI_1 <- read.xlsx(xlsxFile = EDI_dir, sheet = 3,fillMergedCells = T)
 
 # Test
 EDI_1$`#.pregunta.pivote` <-  gsub(pattern = '\n', '',EDI_1$`#.pregunta.pivote`)
@@ -38,7 +38,7 @@ EDI_1$`#.pregunta.pivote` <-  gsub(pattern = '\n', '',EDI_1$`#.pregunta.pivote`)
 nested_EDI_1 <- data.frame(code=EDI_1$Codigo,stringsAsFactors = F, qs=EDI_1$`#.pregunta.pivote`)
 
 # Obtain row of questions
-row_temp <- unlist(strsplit(nested_EDI_1$qs[19],split = ';'))
+row_temp <- unlist(strsplit(nested_EDI_1$qs[2],split = ';'))
 row_temp <-  row_temp[row_temp != ""]
 # Match column names of global to ieth row
 colums_temp <- global[,row_temp]
@@ -48,15 +48,43 @@ if ( is.data.frame(colums_temp) ) {
     new_col_temp <- rowSums(sweep(colums_temp, 2, vector_1, "*"),na.rm = T)
   }
   else if (all(names(colums_temp) == case_2)) {
-    colums_temp$`115F` <- 
+    colums_temp$`115E`[is.na(colums_temp$`115E`)] <- 0
+    colums_temp$`115D`[is.na(colums_temp$`115D`)] <- 0
+    SUB_115CA <- colums_temp$`115C` / colums_temp$`115A`
+    SUB_115CA[is.na(SUB_115CA)] <- 0
+    
+    SUB_115BA <- colums_temp$`115B` / colums_temp$`115A`
+    SUB_115BA[is.na(SUB_115BA)] <- 0
+    # Operation
+    SUB_115BA[SUB_115BA > 1] <- 1
+    SUB_115CA[SUB_115CA > 1] <- 1
+    colums_temp$`115D`[colums_temp$`115D` > 0] <- 1
+    colums_temp$`115E`[colums_temp$`115E` > 0] <- 1
+    
+    new_col_temp <- (SUB_115BA+SUB_115CA+colums_temp$`115D`+colums_temp$`115E`)/4
   }
+  
+  else if(all(names(colums_temp) == case_3)) {
+    new_col_temp <- (rowSums(sweep(colums_temp, 2, vector_3, "*"),na.rm = T))/6
+  }
+  
+  else if(all(names(colums_temp) == case_4)) {
+    new_col_temp <- (rowSums(sweep(colums_temp, 2, vector_4, "*"),na.rm = T))/3
+  }
+  
   
   else {
     new_col_temp <- rowMeans(colums_temp, na.rm=TRUE)
   }
   
 } else {
-  new_col_temp <- as.numeric(colums_temp)
+  if (all(names(row_temp) == case_5)) {
+    case_5.1 <- colums_temp - 1
+    new_col_temp <- as.numeric(case_5.1)
+  }else{
+    new_col_temp <- as.numeric(colums_temp)
+  }
+  
 }
 
 
@@ -99,6 +127,20 @@ to_save_2 = c()
 # Empty list to keep important objects
 to_keep = c()
 
+# Special cases
+case_1 <-  c('I31A','I31B','I31C','I31D','I31E')
+vector_1 <- c(1,0.75,0.5,0.25,0)
+
+case_2 <- c("115A", "115B", "115C", "115D", "115E", "115F")
+
+case_3 <- c("3C1","3C2","3D1","3D2","3E1","3E2","3F1","3F2","3G1","3G2","3H1","3H2")
+vector_3 <- c(1,0.5,1,0.5,1,0.5,1,0.5,1,0.5,1,0.5)
+
+case_4 <- c("3A1","3A2","3C1","3C2","3H1","3H2")
+vector_4 <- c(1,0.5,1,0.5,1,0.5)
+
+case_5 <- "334B"
+
 # For the 4 pilars
 
 for (pilar in 1:4) {
@@ -120,9 +162,47 @@ for (pilar in 1:4) {
     colums_temp <- global[,row_temp]
     # Means of the group
     if ( is.data.frame(colums_temp) ) {
-      new_col_temp <- rowMeans(colums_temp, na.rm=TRUE)
+      if (all(names(colums_temp) == case_1)) {
+        new_col_temp <- rowSums(sweep(colums_temp, 2, vector_1, "*"),na.rm = T)
+      }
+      else if (all(names(colums_temp) == case_2)) {
+        colums_temp$`115E`[is.na(colums_temp$`115E`)] <- 0
+        colums_temp$`115D`[is.na(colums_temp$`115D`)] <- 0
+        SUB_115CA <- colums_temp$`115C` / colums_temp$`115A`
+        SUB_115CA[is.na(SUB_115CA)] <- 0
+        
+        SUB_115BA <- colums_temp$`115B` / colums_temp$`115A`
+        SUB_115BA[is.na(SUB_115BA)] <- 0
+        # Operation
+        SUB_115BA[SUB_115BA > 1] <- 1
+        SUB_115CA[SUB_115CA > 1] <- 1
+        colums_temp$`115D`[colums_temp$`115D` > 0] <- 1
+        colums_temp$`115E`[colums_temp$`115E` > 0] <- 1
+        
+        new_col_temp <- (SUB_115BA+SUB_115CA+colums_temp$`115D`+colums_temp$`115E`)/4
+      }
+      
+      else if(all(names(colums_temp) == case_3)) {
+        new_col_temp <- (rowSums(sweep(colums_temp, 2, vector_3, "*"),na.rm = T))/6
+      }
+      
+      else if(all(names(colums_temp) == case_4)) {
+        new_col_temp <- (rowSums(sweep(colums_temp, 2, vector_4, "*"),na.rm = T))/3
+      }
+      
+      
+      else {
+        new_col_temp <- rowMeans(colums_temp, na.rm=TRUE)
+      }
+      
     } else {
-      new_col_temp <- as.numeric(colums_temp)
+      if (all(names(row_temp) == case_5)) {
+        case_5.1 <- 1- colums_temp
+        new_col_temp <- as.numeric(case_5.1)
+      }else{
+        new_col_temp <- as.numeric(colums_temp)
+      }
+      
     }
     
     container[[i]] <- new_col_temp
@@ -152,8 +232,6 @@ for (pilar in 1:4) {
   # Important object for last computation
   to_keep[[pilar]] <- group_means_2
   
-  
-  
 }
 
 library(rlist)
@@ -163,14 +241,3 @@ final_df <- data.frame("índice" = rowMeans(almost_final_df))
 final_df <- cbind(names, final_df)
 
 
-
-
-# Special cases
-case_1 <-  c('I31A','I31B','I31C','I31D','I31E')
-vector_1 <- c(1,0.75,0.5,0.25,0)
-
-case_2 <- c("115A", "115B", "115C", "115D", "115E", "115F")
-
-
-
-rowSums(sweep(colums_temp, 2, vector_1, "*"),na.rm = T)
